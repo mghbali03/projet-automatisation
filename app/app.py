8ae2d295ccb456a547076689e074d1f153fdd563
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2026-04-21 15:48:26 +0100

    add database to the project

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+from db import courses_collection
 
 app = Flask(__name__)
 
-formations = [
-    {"id": 1, "titre": "DevOps & CI/CD", "duree": "40h", "niveau": "Intermédiaire"},
-    {"id": 2, "titre": "Docker & Kubernetes", "duree": "30h", "niveau": "Avancé"},
-    {"id": 3, "titre": "Python pour le Web", "duree": "25h", "niveau": "Débutant"},
-    {"id": 4, "titre": "Linux Administration", "duree": "20h", "niveau": "Intermédiaire"},
-    {"id": 5, "titre": "Sécurité Réseau", "duree": "35h", "niveau": "Avancé"},
-    {"id": 6, "titre": "SEO", "duree": "20h", "niveau": "Intermédiaire"},
-]
+
+# formations = [
+#     {"id": 1, "titre": "DevOps & CI/CD", "duree": "40h", "niveau": "Intermédiaire"},
+#     {"id": 2, "titre": "Docker & Kubernetes", "duree": "30h", "niveau": "Avancé"},
+#     {"id": 3, "titre": "Python pour le Web", "duree": "25h", "niveau": "Débutant"},
+#     {"id": 4, "titre": "Linux Administration", "duree": "20h", "niveau": "Intermédiaire"},
+#     {"id": 5, "titre": "Sécurité Réseau", "duree": "35h", "niveau": "Avancé"},
+#     {"id": 6, "titre": "SEO", "duree": "20h", "niveau": "Intermédiaire"},
+# ]
 
 @app.route("/")
 def home():
@@ -17,7 +19,8 @@ def home():
 
 @app.route("/formations")
 def formations_page():
-    return render_template("Formations.html", formations=formations)
+    courses = list(courses_collection.find())
+    return render_template("Formations.html", formations=courses)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3002, debug=False)
